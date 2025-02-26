d0b0a97efd7a3825b77a4433c126e7d756d823bd
Rahul Joshi <joshirahul7040@gmail.com>
2025-02-26 14:11:46 +0530

    First commit

---
+++ Server/src/main/java/com/example/SpringAi_gen/SpringAiGenApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringAi_gen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiGenApplication.class, args);
+	}
+
+}
---
+++ Server/src/test/java/com/example/SpringAi_gen/SpringAiGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringAi_gen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
